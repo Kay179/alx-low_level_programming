cccf415545fa34bd34c1d61252cc8a8b77f9048e
Kay179 <mwiingakateule@gmail.com>
2023-02-28 11:07:51 +0200

    changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints "Programming is like building a multilingual 
  * puzzle, followed by a newline.
@@ -8,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return(0);
 }
