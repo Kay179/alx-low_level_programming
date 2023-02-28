ae2c4bb6f85f6969240b390a5c5eb3778d30784d
Kay179 <mwiingakateule@gmail.com>
2023-02-28 16:56:11 +0200

    a

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * void - swaps values of a and b in main function using pointers.
+ * swap_int - swaps values of a and b in main function using pointers.
  * @a: integer value
  * @b: integer value
  * Return: nothing
