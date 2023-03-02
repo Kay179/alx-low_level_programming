27811eb02d41df74c08021dea0f816eb09e36934
Kay179 <mwiingakateule@gmail.com>
2023-03-02 21:27:41 +0200

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints bae 16 in lowercase 
+ * Return: 0
+ */
+int main(void)
+{
+	int base16, lett;
+
+	for (base16 = '0'; base16 <= '9'; base16++)
+	{
+		putchar(base16);
+	}
+	for (lett = 'a'; lett <= 'f'; lett++)
+	{
+		putchar(lett);
+	}
+	putchar('\n');
+	return (0);
+}
