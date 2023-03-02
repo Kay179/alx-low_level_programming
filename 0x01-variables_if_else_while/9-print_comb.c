30bba3481dac26d2acec94eb1f7d9eefbcaa66ab
Kay179 <mwiingakateule@gmail.com>
2023-03-02 21:36:48 +0200

    weird

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers with commas basically
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
