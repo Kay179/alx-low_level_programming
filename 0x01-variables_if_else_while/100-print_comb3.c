7af937b77ad1e251254f61b6b34878206affe355
Kay179 <mwiingakateule@gmail.com>
2023-03-02 21:59:46 +0200

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints combo of numbers 
+ * Returns: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
